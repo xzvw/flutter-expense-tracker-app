7f3b31b356029e17a2180f4b835c774d757ace54
xzvw <128382145+xzvw@users.noreply.github.com>
2023-05-23 23:39:40 +0800

    Add dummy expense data

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_expense_tracker_app/models/expense.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -10,6 +12,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<Expense> dummyExpenseList = [
+    Expense(
+      title: 'Title1',
+      amount: 10.00,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Title2',
+      amount: 20.00,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Title3',
+      amount: 30.00,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+  ];
+
   @override
   Widget build(context) {
     return Scaffold(
